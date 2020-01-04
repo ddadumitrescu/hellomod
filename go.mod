360c26de189d6c7413040ce6137a4e0bafef2617
andrei <someone@someplace.com>
2020-01-04 23:22:30 +0200

    some init msg

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ddadumitrescu/hellomod
+
+go 1.13
---
+++ hellomod.go
@@ -0,0 +1,13 @@
+package hellomod
+
+import "fmt"
+
+// SayHello says `Hello Go!` in English
+func SayHello() {
+	fmt.Println("Hello Go!")
+}
+
+// Salut says `Hello Go!` in Romanian
+func Salut() {
+	fmt.Println("Salut Go!")
+}
